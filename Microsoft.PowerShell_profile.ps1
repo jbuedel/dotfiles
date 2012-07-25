0c69d2f0a8f234db19ea13e410240aae21931084
Josh Buedel <jbuedel@gmail.com>
2012-07-25 13:54:35 -0500

    posh-josh updates. Added a 'touch' command (unfortunately directly in profile.ps1). Added a script to send notifications to Growl (not using it yet though).

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
+set-alias bc 'C:\Program Files (x86)\Beyond Compare 3\BComp.exe'
+
 Import-Module posh-josh
 Import-Module posh-git
 Import-Module project-commands
@@ -20,7 +22,8 @@ Set-Alias rubymine "C:\Program Files (x86)\JetBrains\RubyMine 4.0\bin\rubymine.e
 Set-Alias rcsi "C:\Program Files (x86)\Microsoft Codename Roslyn CTP\Binaries\rcsi.exe"
 Set-Alias gitex 'C:\Program Files (x86)\GitExtensions\gitex.cmd'
 Set-Alias linqpad 'C:\Program Files (x86)\LINQPad4\LINQPad.exe'
- 
+Set-Alias rubymine 'C:\Program Files (x86)\JetBrains\RubyMine 4.0.3\bin\rubymine.exe' 
+
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
@@ -63,3 +66,80 @@ function foo {
 		return $pwd
 	}
 }
+
+function Set-FileTime{
+  param(
+    [string[]]$paths,
+    [bool]$only_modification = $false,
+    [bool]$only_access = $false
+  );
+
+  begin {
+    function updateFileSystemInfo([System.IO.FileSystemInfo]$fsInfo) {
+      $datetime = get-date
+      if ( $only_access )
+      {
+         $fsInfo.LastAccessTime = $datetime
+      }
+      elseif ( $only_modification )
+      {
+         $fsInfo.LastWriteTime = $datetime
+      }
+      else
+      {
+         $fsInfo.CreationTime = $datetime
+         $fsInfo.LastWriteTime = $datetime
+         $fsInfo.LastAccessTime = $datetime
+       }
+    }
+   
+    function touchExistingFile($arg) {
+      if ($arg -is [System.IO.FileSystemInfo]) {
+        updateFileSystemInfo($arg)
+      }
+      else {
+        $resolvedPaths = resolve-path $arg
+        foreach ($rpath in $resolvedPaths) {
+          if (test-path -type Container $rpath) {
+            $fsInfo = new-object System.IO.DirectoryInfo($rpath)
+          }
+          else {
+            $fsInfo = new-object System.IO.FileInfo($rpath)
+          }
+          updateFileSystemInfo($fsInfo)
+        }
+      }
+    }
+   
+    function touchNewFile([string]$path) {
+      #$null > $path
+      Set-Content -Path $path -value $null;
+    }
+  }
+ 
+  process {
+    if ($_) {
+      if (test-path $_) {
+        touchExistingFile($_)
+      }
+      else {
+        touchNewFile($_)
+      }
+    }
+  }
+ 
+  end {
+    if ($paths) {
+      foreach ($path in $paths) {
+        if (test-path $path) {
+          touchExistingFile($path)
+        }
+        else {
+          touchNewFile($path)
+        }
+      }
+    }
+  }
+}
+
+New-Alias touch Set-FileTime
