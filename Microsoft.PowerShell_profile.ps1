d4490da858bc441127acf23dbadedfd9d86b3e21
Josh Buedel <jbuedel@gmail.com>
2013-11-26 14:25:14 -0600

    Added another server.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -87,7 +87,7 @@ function foo {
      Launches an rdp session to www.fpweb.net.
 #>
 function rdp {
-    param([ValidateSet("old_www_box","dev.fpweb.net","ampdev.net","www1","www2", "mercury.fpweb.net")][string]$server)
+    param([ValidateSet("old_www_box","dev.fpweb.net","ampdev.net","www1","www2", "mercury.fpweb.net","orchestrator")][string]$server)
 
     $the_server = $server # $server can only be one of set values.
 
@@ -95,6 +95,7 @@ function rdp {
     if($server -eq "www1") { $the_server = "172.27.0.53" }
     if($server -eq "www2") { $the_server = "172.27.0.67" }
 	if($server -eq "old_www_box") { $the_server = "204.144.122.42" }
+    if($server -eq "orchestrator") { $the_server = "172.27.10.30" }
 
     Start-RDP -Server $the_server -Fullscreen
 }
