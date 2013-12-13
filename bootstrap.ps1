e3bf08d02807823ec0abf19170433fadaf98e77a
Josh Buedel <jbuedel@gmail.com>
2013-12-13 15:26:05 -0600

    Update bootstrap.ps1
    Create powershell directory if necessary.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,9 @@
 # Intended to install as much of my powershell customizations as possible on a new machine.
 
+# Create ~\Documents\WindowsPowerShell
+ if(!(test-path $profile\..)) {mkdir $profile\.. }
+ cd $profile\..
+
 # Install psget - the NuGet of Powershell
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 
