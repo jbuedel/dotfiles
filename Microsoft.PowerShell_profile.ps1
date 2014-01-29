e26a23106000a225ef9ddfecc4a65538aa1962b6
Josh Buedel <jbuedel@gmail.com>
2014-01-29 09:13:42 -0600

    Added lansweeper machine to rdp.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -85,7 +85,7 @@ function foo {
      Launches an rdp session to www.fpweb.net.
 #>
 function rdp {
-    param([ValidateSet("old_www_box","dev.fpweb.net","ampdev.net","www1","www2", "mercury.fpweb.net","orchestrator","tickets.fpweb.net","vmm")][string]$server)
+    param([ValidateSet("old_www_box","dev.fpweb.net","ampdev.net","www1","www2", "mercury.fpweb.net","orchestrator","tickets.fpweb.net","vmm","lansweeper")][string]$server)
 
     $the_server = $server # $server can only be one of set values.
 
@@ -95,6 +95,7 @@ function rdp {
 	if($server -eq "old_www_box") { $the_server = "204.144.122.42" }
     if($server -eq "orchestrator") { $the_server = "172.27.10.30" }
     if($server -eq "vmm") { $the_server = "172.27.10.10" }
+    if($server -eq "lansweeper") { $the_server = "172.27.10.12" }
 
     Start-RDP -Server $the_server -Fullscreen
 }
