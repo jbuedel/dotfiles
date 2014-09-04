5227e5d7bd1fdcc96f3f40a4aa582386eac45dd3
Josh Buedel <jbuedel@gmail.com>
2014-09-04 10:06:51 -0500

    Moved the symlink creation to it's own script for easier execution on existing boxes.

--- bootstrap-newpc.ps1
+++ bootstrap-newpc.ps1
@@ -32,7 +32,3 @@ cinst autohotkey
 # Doesn't exist.
 #cinst webmatrix
 
-#Set up symlinks to my .dotfiles
-new-symlink ~\.vimrc ~\Documents\WindowsPowerShell\.vimrc
-new-symlink ~\.gvimrc ~\Documents\WindowsPowerShell\.gvimrc
-new-symlink ~\.vsvimrc ~\Documents\WindowsPowerShell\.vsvimrc
---
+++ bootstrap-symlinks.ps1
@@ -0,0 +1,4 @@
+#Set up symlinks to my .dotfiles
+new-symlink ~\.vimrc ~\Documents\WindowsPowerShell\.vimrc
+new-symlink ~\.gvimrc ~\Documents\WindowsPowerShell\.gvimrc
+new-symlink ~\.vsvimrc ~\Documents\WindowsPowerShell\.vsvimrc
