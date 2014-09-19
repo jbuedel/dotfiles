179ad46c1d006644932debca8ae7204715fbd3d0
Josh Buedel <jbuedel@gmail.com>
2014-09-19 11:53:55 -0500

    Added auto hot key script to my dot file.

--- bootstrap-symlinks.ps1
+++ bootstrap-symlinks.ps1
@@ -2,3 +2,5 @@
 new-symlink ~\.vimrc ~\Documents\WindowsPowerShell\.vimrc
 new-symlink ~\.gvimrc ~\Documents\WindowsPowerShell\.gvimrc
 new-symlink ~\.vsvimrc ~\Documents\WindowsPowerShell\.vsvimrc
+; AutoHotKey looks in ~\Documents by default.
+new-symlink ~\Documents\AutoHotKey.ahk ~\Documents\WindowsPowerShell\AutoHotKey.ahk
