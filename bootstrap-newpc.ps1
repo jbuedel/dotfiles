630849afc4e910f40bb3393131c95bd84bd78e4f
Josh Buedel <jbuedel@gmail.com>
2016-01-05 10:28:02 -0600

    Added a nifty screen grabber.

--- bootstrap-newpc.ps1
+++ bootstrap-newpc.ps1
@@ -19,7 +19,7 @@ choco install windirstat
 choco install itunes
 choco install conemu
 choco install EthanBrown.ConEmuConfig
-choco install hipchat
+choco install screentogif
 
 choco install linqpad4
 choco install sysinternals
