31e6758911daff194b330809ad54fa899ec82b79
Josh Buedel <jbuedel@gmail.com>
2016-05-24 22:09:02 -0500

    More idempotent.

--- bootstrap-vim.ps1
+++ bootstrap-vim.ps1
@@ -1,23 +1,27 @@
-# Create vim's temp directory (as specified in my .vimrc)
+# Create vim's temp directory (is used in my .vimrc)
 md ~/vimtmp
 
 # Install Pathogen for vim plugin management
-md ~/vimfiles/autoload 
-md ~/vimfiles/bundle
+if(test-path ~/vimfiles/autoload){} else {
+  md ~/vimfiles/autoload 
+}
+if(test-path ~/vimfiles/bundle) {} else {
+  md ~/vimfiles/bundle
+}
 
-# Install pathogen.
+write-host "Downloading pathogen.vim..."
 # This puts DOS line endings, and vim gets an error when loading!
 (New-Object Net.WebClient).DownloadString("https://tpo.pe/pathogen.vim") > ~/vimfiles/autoload/pathogen.vim
 
 pushd ~/vimfiles/bundle
 
-# Install Solarized theme.
+write-host "Install Solarized theme..."
 git clone git://github.com/altercation/vim-colors-solarized.git
 
-# Powershell syntax highlighting.
+write-host "Powershell syntax highlighting..."
 git clone https://github.com/pprovost/vim-ps1.git
 
-# Omnisharp for .net dev - unfortunately does not work as my vim does not have +python support
+write-host "Omnisharp for .net dev - unfortunately does not work as my vim does not have +python support..."
 git clone https://github.com/OmniSharp/omnisharp-vim.git
 cd omnisharp
 git submodule update --init --recursive
@@ -29,3 +33,4 @@ choco install python2 /y
 choco install python2-x86_32
 
 popd
+
