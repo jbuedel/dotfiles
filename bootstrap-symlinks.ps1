c864504963ecba40145b8b2bad627065a84435fd
Josh Buedel <jbuedel@gmail.com>
2017-10-19 10:05:54 -0500

    Sym link setup doesn't assume the location of the powershell directory.

--- bootstrap-symlinks.ps1
+++ bootstrap-symlinks.ps1
@@ -1,6 +1,9 @@
-#Set up symlinks to my .dotfiles
-new-symlink ~\.vimrc ~\Documents\WindowsPowerShell\.vimrc
-new-symlink ~\.gvimrc ~\Documents\WindowsPowerShell\.gvimrc
-new-symlink ~\.vsvimrc ~\Documents\WindowsPowerShell\.vsvimrc
-; AutoHotKey looks in ~\Documents by default.
-new-symlink ~\Documents\AutoHotKey.ahk ~\Documents\WindowsPowerShell\AutoHotKey.ahk
+# Set up symlinks to my .dotfiles
+
+new-item -Path ~\.vimrc -ItemType SymbolicLink -Value $PSScriptRoot\.vimrc
+new-item -Path ~\.gvimrc -ItemType SymbolicLink -Value $PSScriptRoot\.gvimrc
+new-item -Path ~\.vsvimrc -ItemType SymbolicLink -Value $PSScriptRoot\.vsvimrc
+
+# AutoHotKey looks in ~\Documents by default.
+# (but where does it look when Documents is on Onedrive? need to look into this probably)
+new-item -Path ~\Documents\AutoHotKey.ahk -ItemType SymbolicLink -Value $PSScriptRoot\AutoHotKey.ahk
